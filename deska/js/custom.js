3410d38c7c015e64647c115572c8c64c4d774d17
Edo <hakobyaneduard@list.ru>
2020-01-26 22:12:30 +0400

    delete all old files

--- deska/js/custom.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-    let menuLeftButton = document.querySelector(".label__burger");
-    let body = document.querySelector("body");
-
-    menuLeftButton.addEventListener("click", function () {
-       body.classList.toggle("active");
-    });
-
-});
